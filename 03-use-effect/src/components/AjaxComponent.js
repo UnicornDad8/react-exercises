139d0b52add413366748dac9029a78650dc75ff7
UnicornDad8 <unicorndad8@gmail.com>
2024-03-17 19:30:53 -0300

    Adding a loading state before fetching users

--- 03-use-effect/src/components/AjaxComponent.js
+++ 03-use-effect/src/components/AjaxComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const AjaxComponent = () => {
   const [usuarios, setUsuarios] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
   const getUsuariosEstaticos = () => {
     setUsuarios([
@@ -42,11 +43,13 @@ const AjaxComponent = () => {
   };
   */
 
-  const getUsuariosAjax = async () => {
-    const peticion = await fetch("https://reqres.in/api/users?page=1");
-    const { data } = await peticion.json();
-
-    setUsuarios(data);
+  const getUsuariosAjax = () => {
+    setTimeout(async () => {
+      const peticion = await fetch("https://reqres.in/api/users?page=1");
+      const { data } = await peticion.json();
+      setUsuarios(data);
+      setCargando(false);
+    }, 3000);
   };
 
   useEffect(() => {
@@ -55,20 +58,30 @@ const AjaxComponent = () => {
     getUsuariosAjax();
   }, []);
 
-  return (
-    <div>
-      <h2>Listado de usuarios via AJAX</h2>
-      <ol className="usuarios">
-        {usuarios.map((usuario) => {
-          return (
-            <li key={usuario.id}>
-              {usuario.first_name} {usuario.last_name}
-            </li>
-          );
-        })}
-      </ol>
-    </div>
-  );
+  if (cargando === true) {
+    return (
+      <div className="cargando">
+        <h3>Cargando datos...</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Listado de usuarios via AJAX</h2>
+        <ol className="usuarios">
+          {usuarios.map((usuario) => {
+            return (
+              <li key={usuario.id}>
+                <img src={usuario.avatar} alt={usuario.first_name} width="40" />{" "}
+                &nbsp;
+                {usuario.first_name} {usuario.last_name}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 };
 
 export default AjaxComponent;
