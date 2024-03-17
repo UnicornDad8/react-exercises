5bfa5a59dc2f302a25640e7857624f8487566439
UnicornDad8 <unicorndad8@gmail.com>
2024-03-17 13:03:44 -0300

    Added README.md file

--- 03-use-effect/src/components/PruebasComponent.js
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const PruebasComponent = () => {
+  const [usuario, setUsuario] = useState("Ceci Benitez");
+  const [fecha, setFecha] = useState("04-02-1993");
+
+  const modificarUsuario = (e) => {
+    setUsuario(e.target.value);
+  };
+
+  const cambiarFecha = () => {
+    setFecha(Date.now());
+  };
+
+  // se ejecuta solo al cargar el componente
+  useEffect(() => {
+    console.log("Has cargado el componente");
+  }, []);
+
+  // se ejecuta solo al cambiar el nombre de usuario
+  useEffect(() => {
+    console.log("Has cambiado el nombre de usuario");
+  }, [usuario]);
+
   return (
     <div>
       <h2>El efecto - Hook useEffect</h2>
+      <strong className="label">{usuario}</strong>
+      <strong>{fecha}</strong>
+      <div>
+        <input
+          type="text"
+          onChange={modificarUsuario}
+          placeholder="Cambiar nombre de usuario"
+        />
+        <button onClick={cambiarFecha}>Cambiar Fecha</button>
+      </div>
     </div>
   );
 };
---
+++ README.md
@@ -0,0 +1,33 @@
+## React Exercises
+
+> This is a recopilation of react exercises from Victor Robles course on Udemy.
+
+## 🔧 Built With
+
+- JSX
+- CSS
+- React
+
+## 🛠 Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+- Go to the main page of te repo.
+- Press the `Code` button and get the repo link.
+- Clone it using git.
+
+## ✒️ Authors
+
+👤 **Unicorn Dad**
+
+- [@UnicornDad8](https://github.com/UnicornDad8)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/UnicornDad8/react-exercises/issues).
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
