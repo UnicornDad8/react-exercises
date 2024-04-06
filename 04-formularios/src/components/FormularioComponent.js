1f36ca658c2b68a74558ef16446099607bf1d668
UnicornDad8 <unicorndad8@gmail.com>
2024-04-06 15:21:59 -0400

    Manejo basico de onSubmit input con datos de formulario

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormularioComponent = () => {
+  const [usuario, setUsuario] = useState({});
+
+  const conseguirDatosFormulario = (e) => {
+    e.preventDefault();
+
+    let datos = e.target;
+    let usuario = {
+      nombre: datos.nombre.value,
+      apellido: datos.apellido.value,
+      genero: datos.genero.value,
+      bio: datos.bio.value,
+    };
+    console.log(usuario);
+
+    setUsuario(usuario);
+  };
+
   return (
     <div>
       <h2>Formularios con React</h2>
-      <form>
-        <input type="text" placeholder="Nombre" />
-        <input type="text" placeholder="Apellido" />
-        <select>
+      {usuario.bio && usuario.bio.length >= 1 && (
+        <div className="info_usuario label label-gray">
+          <p>
+            {usuario.nombre} {usuario.apellido}
+          </p>
+          <p>{usuario.genero}</p>
+          <p>{usuario.bio}</p>
+        </div>
+      )}
+      <form onSubmit={conseguirDatosFormulario}>
+        <input type="text" placeholder="Nombre" name="nombre" />
+        <input type="text" placeholder="Apellido" name="apellido" />
+        <select name="genero">
           <option value="hombre">Hombre</option>
           <option value="mujer">Mujer</option>
         </select>
-        <textarea placeholder="Biografia" />
+        <textarea placeholder="Biografia" name="bio" />
         <input type="submit" value="Enviar" />
       </form>
     </div>
