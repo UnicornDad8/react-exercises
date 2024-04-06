f55b7e198d61eee973324445cdd76694989c64d4
UnicornDad8 <unicorndad8@gmail.com>
2024-04-06 17:15:16 -0400

    Trabajando con el evento onChange en el formulario

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -12,16 +12,25 @@ const FormularioComponent = () => {
       apellido: datos.apellido.value,
       genero: datos.genero.value,
       bio: datos.bio.value,
+      enviar: datos.enviar.value,
     };
-    console.log(usuario);
 
     setUsuario(usuario);
+  };
+
+  const cambiarDatos = (e) => {
+    let name_del_input = e.target.name;
+
+    setUsuario((estado_previo) => ({
+      ...estado_previo,
+      [name_del_input]: e.target.value,
+    }));
   };
 
   return (
     <div>
       <h2>Formularios con React</h2>
-      {usuario.bio && usuario.bio.length >= 1 && (
+      {usuario.enviar && (
         <div className="info_usuario label label-gray">
           <p>
             {usuario.nombre} {usuario.apellido}
@@ -31,14 +40,24 @@ const FormularioComponent = () => {
         </div>
       )}
       <form onSubmit={conseguirDatosFormulario}>
-        <input type="text" placeholder="Nombre" name="nombre" />
-        <input type="text" placeholder="Apellido" name="apellido" />
-        <select name="genero">
+        <input
+          type="text"
+          placeholder="Nombre"
+          name="nombre"
+          onChange={cambiarDatos}
+        />
+        <input
+          type="text"
+          placeholder="Apellido"
+          name="apellido"
+          onChange={cambiarDatos}
+        />
+        <select name="genero" onChange={cambiarDatos}>
           <option value="hombre">Hombre</option>
           <option value="mujer">Mujer</option>
         </select>
-        <textarea placeholder="Biografia" name="bio" />
-        <input type="submit" value="Enviar" />
+        <textarea placeholder="Biografia" name="bio" onChange={cambiarDatos} />
+        <input type="submit" value="Enviar" name="enviar" />
       </form>
     </div>
   );
