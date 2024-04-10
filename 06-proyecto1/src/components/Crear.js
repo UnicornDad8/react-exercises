1d60ac674513a4c1bf4f872f48cbd7df0de5cfea
UnicornDad8 <unicorndad8@gmail.com>
2024-04-10 14:24:53 -0400

    Creando el formulario para agregar peliculas

--- 06-proyecto1/src/components/Crear.js
+++ 06-proyecto1/src/components/Crear.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Crear = () => {
+  const tituloComponente = "Añadir Pelicula";
+  const [peliState, setPeliState] = useState({
+    titulo: "",
+    descripcion: "",
+  });
+
+  const { titulo, descripcion } = peliState;
+
+  const conseguirDatosForm = (e) => {
+    e.preventDefault();
+
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion,
+    };
+    setPeliState(peli);
+
+    console.log(peliState);
+  };
+
   return (
     <div className="add">
-      <h3 className="title">Añadir Pelicula</h3>
-      <form>
-        <input type="text" placeholder="Titulo" />
-        <textarea placeholder="Descripcion"></textarea>
-        <input type="submit" value="Guardar" />
+      <h3 className="title">{tituloComponente}</h3>
+      {titulo && descripcion && (
+        <p>
+          Has creado la pelicula: <strong>{titulo}</strong>
+        </p>
+      )}
+      <form onSubmit={conseguirDatosForm}>
+        <input type="text" placeholder="Titulo" id="titulo" name="titulo" />
+        <textarea
+          id="descripcion"
+          name="descripcion"
+          placeholder="Descripcion"
+        ></textarea>
+        <input type="submit" id="save" value="Guardar" />
       </form>
     </div>
   );
