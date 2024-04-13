ab7502f7168a4ca41c4c28157eac40fefc7c7787
UnicornDad8 <unicorndad8@gmail.com>
2024-04-13 15:15:22 -0400

    Dibujando maquetado de formulario para editar peliculas

--- 06-proyecto1/src/components/Editar.js
+++ 06-proyecto1/src/components/Editar.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Editar = () => {
-  return <div>Editar</div>;
+  const titulo_componente = "Editar Pelicula";
+  return (
+    <div className="edit_form">
+      <h3 className="title">{titulo_componente}</h3>
+      <form className="edit_form">
+        <input
+          type="text"
+          name="titulo"
+          className="titulo_editado"
+          defaultValue="Titulo original de la pelicula"
+        />
+        <textarea
+          name="descripcion"
+          defaultValue="Descripcion original"
+          className="descripcion_editada"
+        />
+        <input type="submit" className="editar" value="Actualizar" />
+      </form>
+    </div>
+  );
 };
 
 export default Editar;
