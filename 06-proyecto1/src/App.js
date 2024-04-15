d963e313216569d2822cb02009a15bdf6a6891c2
UnicornDad8 <unicorndad8@gmail.com>
2024-04-15 14:05:05 -0400

    Agregamos funcionalidad al buscador de peliculas

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -41,7 +41,10 @@ function App() {
       </section>
 
       <aside className="lateral">
-        <Buscador />
+        <Buscador
+          listadoState={listadoState}
+          setListadoState={setListadoState}
+        />
         <Crear setListadoState={setListadoState} />
       </aside>
 
--- 06-proyecto1/src/components/Buscador.js
+++ 06-proyecto1/src/components/Buscador.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Buscador = () => {
+const Buscador = ({ listadoState, setListadoState }) => {
+  const [busqueda, setBusqueda] = useState("");
+  const [noEncontrado, setNoEncontrado] = useState(false);
+
+  const buscarPeli = (e) => {
+    setBusqueda(e.target.value);
+
+    let pelis_encontradas = listadoState.filter((peli) => {
+      return peli.titulo.toLowerCase().includes(busqueda.toLowerCase());
+    });
+
+    if (busqueda.length <= 1 || pelis_encontradas <= 0) {
+      pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
+      setNoEncontrado(true);
+    } else {
+      setNoEncontrado(false);
+    }
+
+    setListadoState(pelis_encontradas);
+  };
+
   return (
     <div className="search">
       <h3 className="title">Buscador</h3>
+      {noEncontrado && busqueda.length > 3 && (
+        <p className="no-encontrado">
+          no se ha encontrado ninguna coincidencia
+        </p>
+      )}
       <form>
-        <input type="text" placeholder="Buscar" />
+        <input
+          type="text"
+          placeholder="Buscar"
+          name="busqueda"
+          autoComplete="off"
+          value={busqueda}
+          onChange={buscarPeli}
+        />
         <button>Buscar</button>
       </form>
     </div>
